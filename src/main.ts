e192eea8927cef593940758c7c2b3aefd370a385
Denis G <denis.gandzii@kronsoft.ro>
2022-08-19 10:50:26 +0300

    Allow all origin

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import * as Process from 'process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ origin: '*' });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(Process.env.PORT || 3000);
 }
